2720861fba4c298f49ba07377d91403d961e65bd
felipe12-work <felipe12.work@gmail.com>
2025-04-09 16:00:56 -0300

    animações
    Animations e Animator.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed;
-
+    private float moveX, moveY;
     public Rigidbody2D rig2D;
 
     // Start is called before the first frame update
@@ -17,6 +17,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Preencher o vetor de movimentação com os inputs dos eixos horizontal e
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        Move();
         
     }
+
+   void Move()
+    {
+        Vector3 direction = new Vector3(moveX, moveY, 0);
+        rig2D.MovePosition(transform.position + direction * moveSpeed * Time.deltaTime);
+    }
+
 }
